61c04019109fd7bbe536eae0311e8635dc4ebb54
itcast <1477853611.qq.com>
2022-09-02 11:30:59 +0800

    引入Eureka cli 依赖，实现负载均衡

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -19,7 +19,7 @@ public class OrderService {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
 
-        String url = "http://localhost:8081/user/" + order.getUserId();
+        String url = "http://user-service/user/" + order.getUserId();
         User user = restTemplate.getForObject(url, User.class);
         order.setUser(user);
 
